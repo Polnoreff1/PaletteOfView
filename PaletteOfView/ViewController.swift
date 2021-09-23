8f78a81e2c75bce17fb28e27fe588ec707a3b917
Andrey <assassin15eagle@gmail.com>
2021-09-23 20:04:35 +0300

    final commit

--- PaletteOfView/ViewController.swift
+++ PaletteOfView/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var RedLabel: UILabel!
+    @IBOutlet var GreenLabel: UILabel!
+    @IBOutlet var BlueLabel: UILabel!
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
+    //MARK: - LyfeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - IBAction
+    @IBAction func RedSlider(_ sender: Any) {
+        changeViewColor()
+        RedLabel.text = String(format: "%.2f", redSliderOutlet.value)
+        GreenLabel.text = String(format: "%.2f", greenSliderOutlet.value)
+        BlueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
+    }
+    
+    //MARK: - Other Func
+    func changeViewColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value), green: CGFloat(greenSliderOutlet.value), blue: CGFloat(blueSliderOutlet.value), alpha: 1)
+    }
 }
 
